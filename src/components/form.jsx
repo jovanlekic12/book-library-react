e91ece22d23f5dc53f5f2f3f43e41cd05dfe67ab
jovanlekic12 <lekicjovan1212@gmail.com>
2024-10-29 16:37:35 +0100

    add react portal

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,35 +1,38 @@
 import { ImCross } from "react-icons/im";
-
+import ReactDOM from "react-dom";
 function Form() {
-  return (
-    <form className="form">
-      <ImCross className="cross__icon" />
-      <div className="input__div">
-        <label htmlFor="title">Title of the book:</label>
-        <input
-          type="text"
-          placeholder="Lord of the Rings"
-          className="form__input"
-        />
-      </div>
-      <div className="input__div">
-        <label htmlFor="author">Author:</label>
-        <input
-          type="text"
-          placeholder="J.R.R Tolkien"
-          className="form__input"
-        />
-      </div>
-      <div className="input__div">
-        <label htmlFor="pages">Number of pages:</label>
-        <input type="text" placeholder="300" className="form__input" />
-      </div>
-      <div className="checkbox__div">
-        <input type="checkbox" className="checkbox" />
-        <h3 className="checkbox__label">Have you read this book?</h3>
-        <button className="submit__btn">Submit</button>
-      </div>
-    </form>
+  return ReactDOM.createPortal(
+    <div className="overlay">
+      <form className="form">
+        <ImCross className="cross__icon" />
+        <div className="input__div">
+          <label htmlFor="title">Title of the book:</label>
+          <input
+            type="text"
+            placeholder="Lord of the Rings"
+            className="form__input"
+          />
+        </div>
+        <div className="input__div">
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            placeholder="J.R.R Tolkien"
+            className="form__input"
+          />
+        </div>
+        <div className="input__div">
+          <label htmlFor="pages">Number of pages:</label>
+          <input type="text" placeholder="300" className="form__input" />
+        </div>
+        <div className="checkbox__div">
+          <input type="checkbox" className="checkbox" />
+          <h3 className="checkbox__label">Have you read this book?</h3>
+          <button className="submit__btn">Submit</button>
+        </div>
+      </form>
+    </div>,
+    document.body
   );
 }
 
