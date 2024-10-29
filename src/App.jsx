2d595e387be8ff705a899878f7b8366fc90c30a9
jovanlekic12 <lekicjovan1212@gmail.com>
2024-10-29 16:03:00 +0100

    add title and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    </>
-  )
+    <main className="main__container">
+      <h1 className="app__title">Book Library</h1>
+      <h3 className="app__description">
+        Use the button bellow to add to your library and watch your collection
+        grow
+      </h3>
+      <button className="new__book__btn">New Book</button>
+    </main>
+  );
 }
 
-export default App
+export default App;
