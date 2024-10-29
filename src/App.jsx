f6cb272875878a25f6f79ee37d8958c9443f28e0
jovanlekic12 <lekicjovan1212@gmail.com>
2024-10-29 16:20:59 +0100

    add form and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Form from "./components/form";
 import "./App.css";
 
 function App() {
@@ -14,6 +13,7 @@ function App() {
         grow
       </h3>
       <button className="new__book__btn">New Book</button>
+      <Form />
     </main>
   );
 }
